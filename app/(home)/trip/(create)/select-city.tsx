e9beefa965d22a762e54dfa5924c1770363cc9fd
Hailey Kim <haileydeveloper@gmail.com>
2024-05-19 08:27:28 +0900

    feat: create trip routes

---
+++ app/(home)/trip/(create)/select-city.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function SelectCityScreen() {
+  return (
+    <View>
+      <Text>select-city</Text>
+    </View>
+  );
+}
---
+++ app/(home)/trip/(create)/select-date.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function SelectDateScreen() {
+  return (
+    <View>
+      <Text>select-date</Text>
+    </View>
+  );
+}
