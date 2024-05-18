305e95209cb2ea8b1f9fe502059fede791a00f52
Hailey Kim <haileydeveloper@gmail.com>
2024-05-19 07:54:06 +0900

    feat: home routes

---
+++ app/(home)/(tabs)/home/(detail)/city-detail.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function CityDetailScreen() {
+  return (
+    <View>
+      <Text>CityDetail</Text>
+    </View>
+  );
+}
---
+++ app/(home)/(tabs)/home/place-trending.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function TrendingPlaceScreen() {
+  return (
+    <View>
+      <Text>TrendingPlace</Text>
+    </View>
+  );
+}
