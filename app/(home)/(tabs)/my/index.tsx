3f1db5dc154adc9bfca54d6a192b54ace9a32f25
Hailey Kim <haileydeveloper@gmail.com>
2024-05-18 21:27:30 +0900

    feat: new routes

---
+++ app/(home)/(tabs)/my/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function MyPageScreen() {
+  return (
+    <View>
+      <Text>my page</Text>
+    </View>
+  );
+}
---
+++ app/(onboarding)/survey.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function SurveyScreen() {
+  return (
+    <View>
+      <Text>survey</Text>
+    </View>
+  );
+}
