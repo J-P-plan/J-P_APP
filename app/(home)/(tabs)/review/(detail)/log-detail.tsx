aacebb2b3f44f9da212165a63483b52fdeeff19b
Hailey Kim <haileydeveloper@gmail.com>
2024-05-19 08:20:16 +0900

    feat: create review routes

---
+++ app/(home)/(tabs)/review/(detail)/log-detail.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function TravelLogDetailScreen() {
+  return (
+    <View>
+      <Text>TravelLogDetail</Text>
+    </View>
+  );
+}
---
+++ app/(home)/(tabs)/review/(detail)/review-detail.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function ReviewDetailScreen() {
+  return (
+    <View>
+      <Text>ReviewDetail</Text>
+    </View>
+  );
+}
