b0d6ce0958f2f0773b245fa6992384b8a76416e9
Hailey Kim <haileydeveloper@gmail.com>
2024-05-29 14:42:33 +0900

    chore: install svg modules and add svg icons

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,8 +1,8 @@
 import { View, SafeAreaView, ScrollView } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
-import Feather from "@expo/vector-icons/Feather";
 import useSearchInput from "@/hooks/common/useSearchInput";
 import HomeSection from "@/components/ui/home/HomeSection";
+import SearchIcon from "@/assets/icons/search.svg";
 import MESSAGE from "@/message/common";
 import COLORS from "@/constants/colors";
 
@@ -13,7 +13,7 @@ export default function HomeScreen() {
     <SafeAreaView tw="flex-1 mb-16">
       <ScrollView tw="px-5 pt-2">
         <View tw="flex-row items-center h-[56px] mb-[28px] px-[22px] rounded-full border-[0.8px] border-gray-200 bg-white">
-          <Feather name="search" size={16.8} color={COLORS.gray[900]} />
+          <SearchIcon />
           <TextInput
             tw="ml-[8px]"
             keyboardType="default"
