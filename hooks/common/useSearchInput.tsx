615de620b2c51cd71eb94089b011e57a88d7060f
Hailey Kim <haileydeveloper@gmail.com>
2024-06-13 19:41:13 +0900

    feat: add router.push() to handleSearch

--- hooks/common/useSearchInput.tsx
+++ hooks/common/useSearchInput.tsx
@@ -1,12 +1,15 @@
+import { useRouter } from "expo-router";
 import { useState } from "react";
 import { TextInputProps } from "react-native";
 import { NativeViewGestureHandlerProps } from "react-native-gesture-handler";
 
 export default function useSearchInput() {
   const [value, setValue] = useState("");
+  const { push } = useRouter();
 
   const handleSearch = () => {
     console.log(value);
+    push("/(tabs)/search");
   };
 
   const inputProps: TextInputProps & NativeViewGestureHandlerProps = {
