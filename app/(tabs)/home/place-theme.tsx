4068f776e15f8fb6676f905924128cbcb9d52df2
Hailey Kim <haileydeveloper@gmail.com>
2024-06-20 20:30:10 +0900

    feat: create PlaceTheme Screen, update PlaceCard

--- app/(tabs)/home/place-theme.tsx
+++ app/(tabs)/home/place-theme.tsx
@@ -1,9 +1,20 @@
-import { View, Text } from "react-native";
+import { SafeAreaView, ScrollView, View } from "react-native";
+import { usePlaceListData } from "@/hooks/query/placeData";
+import PlaceCard from "@/components/ui/home/PlaceCard";
 
-export default function ThemePlaceScreen() {
+export default function PlaceThemeScreen() {
+  const { data: placeList } = usePlaceListData("place-theme");
+
   return (
-    <View>
-      <Text>ThemePlace</Text>
-    </View>
+    <SafeAreaView tw="flex-1 mb-20">
+      <ScrollView tw="pt-2">
+        <View tw="flex-row flex-wrap justify-between w-[343px] mx-auto mb-4">
+          {placeList &&
+            placeList.data.map((place) => (
+              <PlaceCard key={place.id} data={place} type={'theme'} />
+            ))}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
--- components/ui/home/PlaceCard.tsx
+++ components/ui/home/PlaceCard.tsx
@@ -4,16 +4,23 @@ import type { PlaceData } from "@/types/api/place";
 
 interface Props {
   data: PlaceData;
+  type?: "trending" | "theme";
 }
 
-export default function PlaceCard({ data }: Props) {
+export default function PlaceCard({ data, type = "trending" }: Props) {
   return (
     <Link
       href={`/(tabs)/home/(detail)/place/${data.id}`}
-      tw="w-[164px] h-[200px] mb-4"
+      tw="w-[164px] h-[200px] mb-5"
     >
       <View>
-        <View tw="w-[164px] h-[155px] rounded bg-secondary-light" />
+        <View tw="w-[164px] h-[155px] rounded bg-secondary-light">
+          {type === "theme" && (
+            <View tw="absolute top-[15px] right-[17px] flex items-center justify-center w-[42px] h-5 rounded-full bg-white/60">
+              <Text tw="font-normal text-[10px] text-gray-900">{"여행지"}</Text>
+            </View>
+          )}
+        </View>
         <View tw="mt-2 ml-2.5">
           <Text tw="mb-1 font-bold text-sm text-gray-900">{data.name}</Text>
           <Text tw="font-bold text-xs text-gray-600">{data.subName}</Text>
