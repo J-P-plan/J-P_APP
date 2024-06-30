c14539dff135dc18d63fa7c8c960bc5dba0ebd7a
Hailey Kim <haileydeveloper@gmail.com>
2024-06-30 21:23:29 +0900

    feat: add margin option to TagChip

--- components/common/TagChip.tsx
+++ components/common/TagChip.tsx
@@ -1,12 +1,19 @@
+import { cn } from "@/lib/util";
 import { View, Text } from "react-native";
 
 interface Props {
   text: string;
+  margin?: string;
 }
 
-export default function TagChip({ text }: Props) {
+export default function TagChip({ text, margin = "mr-[3px]" }: Props) {
   return (
-    <View tw="mr-[3px] py-[3px] px-2 rounded-full border border-gray-700 bg-white">
+    <View
+      tw={cn(
+        "py-[3px] px-2 rounded-full border border-gray-700 bg-white",
+        margin
+      )}
+    >
       <Text tw="font-normal text-xs text-gray-700">{`#${text}`}</Text>
     </View>
   );
