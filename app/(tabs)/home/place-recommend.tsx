0b4f3469d6b6c1918f5c9f7627bb34f4da046ca9
Hailey Kim <haileydeveloper@gmail.com>
2024-07-01 22:21:37 +0900

    feat: create PlaceRecommend Screen

--- app/(tabs)/home/place-recommend.tsx
+++ app/(tabs)/home/place-recommend.tsx
@@ -1,9 +1,25 @@
-import { View, Text } from "react-native";
+import { SafeAreaView, ScrollView, View } from "react-native";
+import { usePlaceListData } from "@/hooks/query/placeData";
+import Map from "@/components/common/Map";
+import BottomSheet from "@/components/common/BottomSheet";
+import RecommendCard from "@/components/ui/home/list/RecommendCard";
 
-export default function RecommendPlaceScreen() {
+export default function PlaceRecommendScreen() {
+  const { data: placeList } = usePlaceListData("place-trending");
+
   return (
-    <View>
-      <Text>recommend-place-near</Text>
-    </View>
+    <SafeAreaView tw="flex-1 mb-20">
+      <Map />
+      <BottomSheet>
+        <ScrollView tw="mb-56">
+          <View tw="w-[343px] mx-auto">
+            {placeList &&
+              placeList.data.map((place) => (
+                <RecommendCard key={place.id} data={place} />
+              ))}
+          </View>
+        </ScrollView>
+      </BottomSheet>
+    </SafeAreaView>
   );
 }
