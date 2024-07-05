3eb37f22cadc973d19ca10b420accae53bece00c
Hailey Kim <haileydeveloper@gmail.com>
2024-07-05 11:41:17 +0900

    feat: update components (wrap around with View)

--- components/common/LikeCount.tsx
+++ components/common/LikeCount.tsx
@@ -1,4 +1,4 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import HeartIcon from "@/assets/icons/common/heart.svg";
 import { cn } from "@/lib/util";
 
@@ -12,9 +12,9 @@ export default function LikeCount({
   textColor = "text-gray-700",
 }: Props) {
   return (
-    <>
+    <View tw='flex-row items-center'>
       <HeartIcon tw="w-4 h-4 text-point-like" />
       <Text tw={cn("ml-[3px] font-normal text-xs", textColor)}>{count}</Text>
-    </>
+    </View>
   );
 }
