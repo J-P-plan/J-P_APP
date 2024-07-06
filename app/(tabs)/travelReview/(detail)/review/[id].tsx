1c1af99ecfec84cee777a021f81362dba2e484b6
Hailey Kim <haileydeveloper@gmail.com>
2024-07-06 10:46:06 +0900

    feat: add Header on ReviewDetailScreen

--- app/(tabs)/travelReview/(detail)/review/[id].tsx
+++ app/(tabs)/travelReview/(detail)/review/[id].tsx
@@ -1,14 +1,26 @@
 import { View, Text } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import { Stack, useLocalSearchParams } from "expo-router";
 import { useReviewData } from "@/hooks/query/reviewData";
+import TravelReviewHeader from "@/components/ui/headers/TravelReviewHeader";
+import MarkerIcon from "@/assets/icons/common/marker.svg";
 
 export default function ReviewDetailScreen() {
   const { id } = useLocalSearchParams();
   const { data: reviewData } = useReviewData(id as string);
 
   return (
-    <View>
-      <Text>ReviewDetail</Text>
-    </View>
+    <>
+      <Stack.Screen
+        options={{
+          header: () => (
+            <TravelReviewHeader
+              type="review"
+              icon={<MarkerIcon tw="w-[18px] h-[18px] text-gray-900" />}
+              title={"오대산 선재길"} /* replace with real data in reviewData */
+            />
+          ),
+        }}
+      />
+    </>
   );
 }
