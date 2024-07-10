0bf6f3150e5d59cfeb1aedcc39123fa19712b82d
Hailey Kim <haileydeveloper@gmail.com>
2024-07-10 14:31:55 +0900

    feat: get isLoading from useQuery and show LoadingSpinner

--- components/ui/search/IndexScreen.tsx
+++ components/ui/search/IndexScreen.tsx
@@ -3,22 +3,25 @@ import { useTextSearchData } from "@/lib/hooks/query/googlePlaceData";
 import { DismissKeyboardSafeAreaView } from "@/components/common/utils/DismissKeyboard";
 import ContentWrapper from "@/components/common/layout/ContentWrapper";
 import SearchBar from "@/components/common/input/SearchBar";
+import LoadingSpinner from "@/components/common/feedback/LoadingSpinner";
 import NoSearchTerm from "@/components/ui/search/templates/NoSearchTerm";
 import SearchResult from "@/components/ui/search/templates/SearchResult";
 import NoSearchResult from "@/components/ui/search/templates/NoSearchResult";
 
 export default function SearchIndexScreen() {
   const { term }: Partial<{ term: string }> = useLocalSearchParams();
-  const { data: resultData } = useTextSearchData(term);
+  const { data: resultData, isLoading } = useTextSearchData(term);
 
   return (
     <DismissKeyboardSafeAreaView tw="flex-1 mb-20 bg-background">
-      <ContentWrapper>
+      <ContentWrapper tw="flex-1">
         <SearchBar currentValue={term} onSearchScreen />
         {!term ? (
           <NoSearchTerm />
         ) : resultData ? (
           <SearchResult results={resultData.results} />
+        ) : isLoading ? (
+          <LoadingSpinner />
         ) : (
           <NoSearchResult />
         )}
